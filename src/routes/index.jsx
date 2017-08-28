c634f036c8951009ce2640865270d6114da5ec32
Weinan Liu <503304900@qq.com>
2017-08-28 23:25:00 +0800

    init commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "dva";
-import { Card, Icon, Table, Carousel } from "antd-mobile";
-import styles from "./index.less";
+import { Card, Icon, Table, Carousel, NavBar,Modal } from "antd-mobile";
+// import styles from "./index.less";
+import './header.less';
+import fig0 from '../assets/a0.png';
+import fig1 from '../assets/a1.png';
+import fig2 from '../assets/a2.png';
+
+const figs = {
+  fig0, fig1, fig2
+}
 
 const listConfig = [
   {
@@ -18,98 +26,114 @@ const listConfig = [
     sub: "将孩子的idea向世界展示"
   }
 ];
-const cardConfig = [{
-  title: '夯实的课程体系',
-  content: [
-    '前置：打字及基础信息技术',
-    '基础：用编程解决学习问题',
-    '高级：开发图形界面小工具',
-    '班制：10人的小班充分学习'
-  ]
-}, {
-  title: '卓越的核心团队',
-  content: [
-    '家教：150+课时／人',
-    '博文：80+篇博客／人',
-    '编码：5+年开发经验／人',
-    '项目：10+个大中型／人'
-  ]
+
+const docConfig = {
+  doc0: {
+    title: "一站式私塾",
+    sub: "提供每晚和周末托管的一站式服务"
+  },
+  doc1: {
+    title: "各学科全面发展",
+    sub: "小初各个学科都有专门的辅导课程"
+  },
+  doc2: {
+    title: "不辜负孩子的兴趣爱好",
+    sub: "平衡学业，让孩子更快乐的学习和生活"
+  }
 }
-// , {
 
-// }, {
-
-// }
+const colors = [
+  '#8966b1',
+  '#47a9ca',
+  '#df6260',
+  // '#fdbd57',
 ];
+
 const columns = [
   { title: '名称', dataIndex: 'title', key: 'title', width: '1rem' },
-  { title: '课时（45分／课时）', dataIndex: 'a', key: 'a', width: '1rem' },
+  { title: '时间', dataIndex: 'a', key: 'a', width: '2rem' },
   { title: '价格（元）', dataIndex: 'b', key: 'b', width: '1rem' },
-  { title: '特点', dataIndex: 'c', key: 'c', width: '1rem' }
 ];
 
 const data = [
-  { title: '基础打字&信息技术', a: '14', b: '2000', c: '拼音打字和基本信息技术', key: '1' },
-  { title: '编程基础课程（上）', a: '18', b: '3000', c: '基础编程知识，解决各种数学问题', key: '3' },
-  { title: '编程基础课程（下）', a: '18', b: '3000', c: '编程中级知识，完成控制台小程序', key: '4' },
-  { title: '图形化程序开发', a: '35', b: '6500', c: '图形化小程序，为idea共创打下基础', key: '5' },
-  { title: '信息竞赛(长期)', a: '15', b: '3000', c: '长期性的信息竞赛辅导', key: '6' }
+  { title: '晚托班', a: '每天放学4点到6点', b: '2000', key: '1' },
+  { title: '周末托管', a: '周末托管2小时', b: '3000', key: '2' },
+  { title: '周末强化', a: '周末半天', b: '3000', key: '3' },
+  { title: '特色班', a: '35', b: '6500', key: '4' },
 ];
-function Index({ location }) {
-  return (
-    <div className={styles.normal1}>
-      <h1 className={styles.title}>培养会编程的下一代</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>一台电脑，一段历程，成就美好未来</li>
-      </ul>
-      <Carousel
+
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      select: 0,
+      modal1: false
+    };
+  }
+
+  render() {
+    const { select, modal1 } = this.state;
+    return (
+      <div className="normal" style={{background: colors[select]}}>
+         <NavBar
+            leftContent={null}
+            rightContent={<a className="tel" href="tel:18868136291">
+                  联系我们
+                </a>}
+          >
+            
+          </NavBar>
+    {/* infinite */}
+        <Carousel
           className="my-carousel"
-          autoplay={true}
-          infinite
-          selectedIndex={1}
+          autoplay={true}  
+          selectedIndex={select}
           swipeSpeed={35}
+          beforeChange={(from, to) => this.setState({select: to})}
         >
           {listConfig.map((lc, i) =>
-          <li key={i} className={styles["achievement" + i]}>
-            <div className={styles["fig" + i]} />
-            {lc.title}
-            <br />
-            {lc.sub}
-          </li>
-        )}
+            <li key={i} className="my-slide">
+              <div className="fig" style={{backgroundImage:`url(${figs['fig'+i]})`}}/>
+            </li>
+          )}
         </Carousel>
-      <div className={styles.cardDiv}>
-        <div className={styles.bottomLine}>特色介绍</div>
-        {cardConfig.map((cc, i) =>
-          <div className={styles.cards} key={i}>
-            <div className={styles.cardTitle}>{cc.title}</div>
-            <div className={styles.cardDetail}>{cc.content.map((ccc,i)=><div key={i} className={styles.leftFloat} >{ccc}</div>)}</div>
-          </div>
-        )}
-        <div className={styles.bottomLine}>价格一览</div>
-        <div className={styles.tableWrapper}>
-          <Table
-            columns={columns}
-            dataSource={data}
-          />
+        <div className="detail-doc">
+          <p className="title">{docConfig['doc'+select].title}</p>
+          <p className="sub">{docConfig['doc'+select].sub}</p>
         </div>
-        <div className={styles.bottomLine} style={{width:'100%'}}>
-          码代（上虞）地址：谢塘镇易居公馆</div>
-        <div>
-          <a className={styles.grey} href="tel:15867164299">
-              技术支持:杭州乾坤网络科技有限公司
+        <div className="detail-button">
+          <span onClick={()=>this.setState({modal1: true})}>查看课表</span>
+        </div>
+        <div className="footer">
+          <a className="support-info" href="tel:18868136291">
+            技术支持：杭州乾坤网络科技有限公司
               <br />
-              上虞区经理王女士：18868136291
+            上虞区经理王女士：18868136291
             </a>
         </div>
+        <Modal
+          title="课程表"
+          transparent
+          maskClosable={false}
+          visible={this.state.modal1}
+          footer={[{ text: '确定', onPress: ()=>this.setState({modal1: false}) }]}
+        >
+        <Table
+        columns={columns}
+        dataSource={data}
+      />
+      <div className="location">
+        新希望私塾地点：上虞谢塘易居公馆
       </div>
-    </div>
-  );
+        </Modal>
+      </div>
+    );
+  }
 }
 
 Index.propTypes = {
-  location: PropTypes.object.isRequired
+  // location: PropTypes.object.isRequired
 };
 
 function mapStateToProps() {
